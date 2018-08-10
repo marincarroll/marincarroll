f751294a63ca4bb953c6f0ab53bc3802b1f2d371
Marin Carroll <marin@marincarroll.com>
2018-08-10 18:02:43 -0400

    8/10/18

--- js/script.js
+++ js/script.js
@@ -1,33 +1,51 @@
-$(document).ready(function() {
-    
-    function reveal(element) {
-    
-    var d= 0;
-    $(element).each(function() {
-        $(this).delay(d).addClass('reveal');
-        d += 1000000;
+function reveal(elements, initialDelay, speed) {
+    var delay = initialDelay;
+    $(elements).each(function (index) {
+        var element = this;
+        delay = (initialDelay + (index * speed));
+        setTimeout(function () {
+            $(element).addClass('reveal');
+        }, delay);
     });
-    }
+}
     
-    function hide(element) {
-    
-    var d= 0;
-    $(element).each(function() {
-        $(this).delay(d).removeClass('reveal');
-        d += 1000000;
+function hide(elements, initialDelay, speed) {
+    var delay = initialDelay;
+    $(elements).each(function (index) {
+        var element = this;
+        delay = (initialDelay + (index * speed));
+        setTimeout(function () {
+            $(element).removeClass('reveal');
+        }, delay);
     });
-    }
-    
-    reveal('.page > *');
-    
-    $(".nav-toggle").click(function() {
+}
+
+function scrolledTo() {
+    $('.page').each(function(index) {
+        var currentPage = this.id;
+            leftEdge = $(this).position().left,
+            scrollPos = $('main').scrollLeft();
+
+        if (scrollPos >= leftEdge){
+            console.log(index, leftEdge, scrollPos);
+            reveal('#' + currentPage + '> *', 0, 0);
+        }       
+    });
+}
+
+$(document).ready(function () {
+    scrolledTo();
+
+    $(".nav-toggle").click(function () {
         $("#main-nav").toggleClass("open");
         
         if ($("#main-nav").hasClass("open")) {
-            reveal('#main-nav li');
+            reveal('#main-nav li', 100, 65);
         } else {
-            hide('#main-nav li');
+            hide('#main-nav li', 100, 0);
         }
     });
-        
+    
+    $('main').scroll(scrolledTo);
+    $(window).resize(scrolledTo);
 });
