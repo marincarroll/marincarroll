a548b6ad786ba3fbe4a5b5926dd8a11024f1ce8c
Marin @Home <marin@marincarroll.com>
2018-08-09 22:54:27 -0400

    Work from

--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
     $(".nav-toggle").click(function() {
         $("#main-nav").toggleClass("open");
     });
+    
+    var d= 0;
+$('.page > *').each(function() {
+    $(this).delay(d).fadeTo('fast', 1).css('margin-top', 'unset');
+    d += 100;
+});
+
+    $(function() {
+        $('.lazy').lazy();
+    });
+        
 });
