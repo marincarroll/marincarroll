09f98279e975b335df564b5511c6b868327a4c45
Marin @Home <marin@marincarroll.com>
2018-08-12 19:44:52 -0400

    8/12

--- js/script.js
+++ js/script.js
@@ -23,54 +23,83 @@ function hide(elements, initialDelay, speed) {
 function scrolledTo() {
     $('.page').each(function (index) {
         var currentPage = this.id,
-            leftEdge = $(this).children(".contents").offset().left;
-        
-        function positions() {
-            var scrollPos = $('main').width() + $('main').scrollLeft();        
-            if (scrollPos >= leftEdge) {
-                reveal('#' + currentPage + ' .contents > *', 0, 100);
+            leftEdge = $(this).children(".contents").offset().left,
+            rightEdge = leftEdge + $(this).innerWidth();
+        function pagePositions() {
+            var scrollLeft = $('main').scrollLeft(),
+                scrollRight = $('main').width() + scrollLeft,
+                contents = '#' + currentPage + ' .contents > *';
+            if (scrollRight >= leftEdge) {
+                reveal(contents, 0, 100);
                 if (currentPage == "contact") {
-                    $('main').css('border-right','none');
+                    $('main').css('border-right','none'); 
+                    
+                } else {
+                    $('main').css('border-right','10px solid transparent');
                 }
-            }
+            }        
+            /*if (scrollLeft >= rightEdge) {
+                hide(contents, 0, 100);
+            }*/
         } 
         
-        positions();
-        $('main').scroll(positions);
-    });
+        pagePositions();
+        $('main').scroll(pagePositions);
+    }); 
 }
 
-$(document).ready(function () {
+function navBorder() {
+    var navEdge = $('.nav-toggle').offset().left + $('.nav-toggle').height(),
+        contactEdge = $('#contact').offset().left;
+    if (navEdge >= contactEdge) {
+        $('#main-nav').css('border-color', 'initial');
+        $('.nav-toggle').css('border-color', 'initial');
+    } else {
+        $('#main-nav').css('border-color', 'transparent');
+        $('.nav-toggle').css('border-color', 'transparent');
+    }
+}
+
+$(document).ready(function() {
     
     /*$(function() {
-   $("main").mousewheel(function(evt, chg) {
-      this.scrollLeft -= (chg * 1); //need a value to speed up the change
-      evt.preventDefault();
-   });
-});*/
+        $("main").mousewheel(function(evt, chg) {
+            this.scrollLeft -= (chg * 1);
+            evt.preventDefault();
+            console.log(chg);
+        });
+    });*/
     
- $(function() {
-	$('#main-nav a').bind('click',function(event){
-		var $anchor = $(this);
-		
-		$('main').stop().animate({
-			scrollLeft: $($anchor.attr('href')).offset().left
-		}, 900);
-		event.preventDefault();
-	});
-});
-    
-    scrolledTo();
+     $(function() {
+        $('a').bind('click',function(event){
+            var $anchor = $(this);
 
+            $('main').stop().animate({
+                scrollLeft: $($anchor.attr('href')).offset().left
+            }, 900);
+            event.preventDefault();
+        });
+    });
+    
     $(".nav-toggle").click(function () {
+        
         $("#main-nav").toggleClass("open");
         
         if ($("#main-nav").hasClass("open")) {
-            reveal('#main-nav li', 100, 100);
+            reveal('#main-nav li', 100, 100);          
+            setTimeout(function(){
+                navBorder();
+            }, (500 - (500 * contactEdgePercent)));
         } else {
-            hide('#main-nav li', 100, 0);
-        }
+            hide('#main-nav li', 100, 0);         
+            setTimeout(function(){
+                navBorder();
+            }, 500 - (1 - contactEdgePercent));
+        }       
     });
     
-    $(window).resize(scrolledTo);
+    navBorder();
+    scrolledTo();
+    $(window).resize(scrolledTo, navBorder);
+    $('main').scroll(navBorder);
 });
