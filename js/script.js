03c5d1efafcc63bb106cf8bdf5a55aed96ae8e3d
Marin <marin@marincarroll.com>
2018-08-14 17:19:50 -0400

    jquery

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
+//Setup reveal effect.
 function reveal(elements, initialDelay, speed) {
     var delay = initialDelay;
-    $(elements).each(function (index) {
+    $(elements).each(function(index) {
         var element = this;
         delay = (initialDelay + (index * speed));
-        setTimeout(function () {
+        setTimeout(function() {
             $(element).addClass('reveal');
         }, delay);
     });
 }
-    
+
+//Setup hide effect.
 function hide(elements, initialDelay, speed) {
     var delay = initialDelay;
-    $(elements).each(function (index) {
+    $(elements).each(function(index) {
         var element = this;
         delay = (initialDelay + (index * speed));
         setTimeout(function () {
@@ -20,34 +22,29 @@ function hide(elements, initialDelay, speed) {
     });
 }
 
+//Execute reveal/hide on page contents when page is scrolled to.
 function scrolledTo() {
-    $('.page').each(function (index) {
+    $('.page').each(function() {
         var currentPage = this.id,
             leftEdge = $(this).children(".contents").offset().left,
             rightEdge = leftEdge + $(this).innerWidth();
         function pagePositions() {
-            var scrollLeft = $('.container').scrollLeft(),
-                scrollRight = $('.container').width() + scrollLeft,
+            var scrollLeft = $('main').scrollLeft(),
+                scrollRight = $('main').width() + scrollLeft,
                 contents = '#' + currentPage + ' .contents > *';
             if (scrollRight >= leftEdge) {
                 reveal(contents, 0, 100);
-                if (currentPage == "contact") {
-                    //$('main').css('border-right','0px solid transparent'); 
-                    
-                } else {
-                    //$('main').css('border-right','10px solid transparent');
-                }
             }
             /*if (scrollLeft >= rightEdge) {
                 hide(contents, 0, 100);
             }*/
         }
-        
         pagePositions();
-        $('.container').scroll(pagePositions);
+        $('main').scroll(pagePositions);
     });
 }
 
+//Toggle border effect on nav menu when it overlaps contact page.
 function navBorder() {
     var navEdge = $('.nav-toggle').offset().left + $('.nav-toggle').height(),
         contactEdge = $('#contact').offset().left;
@@ -60,52 +57,70 @@ function navBorder() {
     }
 }
 
-function smoothScroll() {
-    $('a[href*=\\#]').each(function (index) {
-        function linkPositions(){
-            var hash = this.hash;
-            leftEdge = $(hash).offset().left;
-        }
-        
-        function scroller() {
-            $(this).on('click', function(event){     
-                event.preventDefault();
-                $('.container').animate({scrollLeft:leftEdge}, 500);
-            });
-            linkPositions();
-        }
-        
-        linkPositions();
-        scroller();
+function scrollSnap() {
+    $('.page:not(:last-child)').each(function(){
+        var nextTarget = $(this).next().position().left;
+        $(this).mousewheel(function(){
+            if(event.deltaY >= 50) {
+                $('main').animate({
+                    scrollLeft: nextTarget
+                }, 700);
+                console.log("scrolled to: ", nextTarget)
+            }
+        });
+    });
+    
+    $('.page:not(:first-child)').each(function(){
+        var prevTarget = $(this).prev().position().left;
+        $(this).mousewheel(function(){
+            if(event.deltaY <= -50) {
+                $('main').animate({
+                    scrollLeft: prevTarget
+                }, 700);
+                console.log("scrolled to: ", prevTarget)
+            }
+        });
     });
 }
 
 $(document).ready(function() {
+
     
-    /*$(function() {
+    //Change vertical scroll to horizontal scroll. 
+    $(function() {      
         $("main").mousewheel(function(evt, chg) {
-            this.scrollLeft -= (chg * 1);
+            this.scrollLeft -= (chg);
             evt.preventDefault();
-            console.log(chg);
         });
-    });*/
+        
+        $(document).mousewheel(function(evt){
+           evt.preventDefault(); 
+        });
+    });
     
-    $(".nav-toggle").click(function () {
-        
+    //Toggle nav menu.
+    $(".nav-toggle").click(function() {
         $("#main-nav").toggleClass("open");
-        
         if ($("#main-nav").hasClass("open")) {
-            reveal('#main-nav li', 100, 100);          
-
+            reveal('#main-nav li', 100, 100);
         } else {
-            hide('#main-nav li', 100, 0);         
-
-        }       
+            hide('#main-nav li', 100, 0);
+        }
+    });
+    
+    //Smooth scroll effect for all internal links.
+    $(document).on('click', 'a[href^="#"]', function(event) {
+        event.preventDefault();
+        var target = $($.attr(this, 'href'));
+        $('main').animate({
+            scrollLeft: target.offset().left - $('main').offset().left + $('main').scrollLeft()
+        }, 700);
     });
     
     navBorder();
     scrolledTo();
-    smoothScroll();
-    $(window).resize(scrolledTo, navBorder, smoothScroll);
-    $('.container').scroll(navBorder);
+    scrollSnap();
+    $(window).resize(scrolledTo, navBorder);
+    $('main').scroll(navBorder);
+    
 });
